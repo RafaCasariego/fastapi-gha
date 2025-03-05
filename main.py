e23b487f580f0cf2c5ea80823234189d62c8662b
Rafa Casariego <raff9530@gmail.com>
2025-03-05 15:57:15 +0100

    Adaptando API para AWS Lambda con Mangum

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "¡Hola, GitHub Actions!"}
+    return {"message": "¡Hola, AWS Lambda!"}
 
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+
+# Para que la API sea compatible con AWS Lambda 
+handler = Mangum(app)
